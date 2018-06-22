bf7d125abc59e74318a2a83863383b694964cf94
yovany <you@example.com>
2018-06-21 23:44:48 -0600

    Solo falta lo de los circulos

--- app/src/main/java/sv/edu/uesocc/ipam2018/mapasandroid/MainActivity.java
+++ app/src/main/java/sv/edu/uesocc/ipam2018/mapasandroid/MainActivity.java
@@ -1,14 +1,21 @@
 package sv.edu.uesocc.ipam2018.mapasandroid;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.Point;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -23,29 +30,81 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap Mapa;
     private Button btnOpciones;
     private Button btnIr;
     private Button btnPosicion;
     private Button btnMostrar;
+    private Button btnIrUES;
+    private EditText latitud;
+    private EditText longitud;
+    private static final int MI_PERMISO = 1;
+    private double lat = 0.0;
+    private double lng = 0.0;
+    private ArrayList<String> latCirculos;
+    private ArrayList<String> lngCirculos;
+    private Set<String> latCirculo;
+    private Set<String> lngCirculo;
+    private boolean doit;
+    private String tamanio;
+    private String color;
+    private String mapa;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        preferences = getSharedPreferences("map_preferences",MODE_PRIVATE);
+        latCirculos = new ArrayList<>();
+        lngCirculos = new ArrayList<>();
+        latCirculo = new HashSet<String>();
+        lngCirculo = new HashSet<String>();
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
-
         mapFragment.getMapAsync(this);
 
-        btnOpciones = (Button)findViewById(R.id.btnOpciones);
+        latCirculo = preferences.getStringSet("latCirculo",null);
+        lngCirculo = preferences.getStringSet("lngCirculo", null);
+        mapa = preferences.getString("tipo","Normal");
+        tamanio = preferences.getString("tamanio", "3000");
+        color = preferences.getString("color", "Blue");
+        lat = Double.parseDouble(preferences.getString("latitud", "13.970263"));
+        lng = Double.parseDouble(preferences.getString("longitud", "-89.574808"));
+        if (latCirculo != null){
+            latCirculos = new ArrayList<String>(latCirculo);
+            lngCirculos = new ArrayList<String>(lngCirculo);
+
+        }
+        Log.d("TIPO DE MAPA",mapa);
+        /*if (savedInstanceState != null) {
+            latCirculos = savedInstanceState.getStringArrayList("latCirculos");
+            lngCirculos = savedInstanceState.getStringArrayList("lngCirculos");
+
+        }*/
+
+        btnOpciones = (Button) findViewById(R.id.btnOpciones);
         btnOpciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cambiarOpciones();
+                Intent intent = new Intent(MainActivity.this, OpcionesMapa.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        btnIrUES = findViewById(R.id.btnUES);
+        btnIrUES.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irUES();
             }
         });
 
@@ -53,11 +112,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         btnIr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                irUES();
+                if ((latitud.getText().toString().isEmpty() || latitud.getText().toString() == null)
+                        && (longitud.getText().toString().isEmpty() || longitud.getText().toString() == null)) {
+                    lat = 0.0;
+                    lng = 0.0;
+                    irPosicion();
+                } else {
+                    lat = Double.parseDouble(latitud.getText().toString());
+                    lng = Double.parseDouble(longitud.getText().toString());
+                    irPosicion();
+                }
+
             }
         });
 
-        btnPosicion = (Button)findViewById(R.id.btnPosicion);
+        btnPosicion = (Button) findViewById(R.id.btnPosicion);
         btnPosicion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,7 +134,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-        btnMostrar = (Button)findViewById(R.id.btnPin);
+        btnMostrar = (Button) findViewById(R.id.btnPin);
         btnMostrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,78 +142,176 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+        latitud = (EditText) findViewById(R.id.latitudtext);
+        longitud = (EditText) findViewById(R.id.longitudtxt);
 
-
-
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MI_PERMISO);
     }
+
+    /*@Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("latitud", latitud.getText().toString());
+        outState.putString("longitud", longitud.getText().toString());
+        outState.putStringArrayList("latCirculos", latCirculos);
+        outState.putStringArrayList("lngCirculos", lngCirculos);
+        super.onSaveInstanceState(outState);
+    }*/
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         Mapa = googleMap;
-
+        mapType();
         Mapa.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             public void onMapClick(LatLng point) {
-
-
                 Toast.makeText(
                         MainActivity.this,
                         "Click\n" +
                                 "Lat: " + point.latitude + "\n" +
-                                "Lng: " + point.longitude + "\n" ,
+                                "Lng: " + point.longitude + "\n",
                         Toast.LENGTH_SHORT).show();
             }
         });
 
         Mapa.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             public void onMapLongClick(LatLng point) {
-
-                Toast.makeText(MainActivity.this, "Punto del Clic: "+ point, Toast.LENGTH_LONG).show();
-                CircleOptions circleOptions = new CircleOptions();
-                circleOptions.center(point);
-                circleOptions.fillColor(Color.HSVToColor(75, new float[]{Color.BLUE, 1, 1}));
-                circleOptions.radius(1000);
-                circleOptions.strokeWidth(1);
-                Circle circulo= Mapa.addCircle(circleOptions);
+                Toast.makeText(MainActivity.this, "Punto del Clic: " + point, Toast.LENGTH_LONG).show();
+                addCirculo(point);
             }
         });
+
+        if (latCirculos != null) {
+            Log.d("COLOCARA PUNTOS","Deberia servir");
+                int i = 0;
+                int tam = latCirculos.size();
+                while (i < tam){
+                    Log.d("LATITUD",latCirculos.get(i));
+                    Log.d("LONGITUD",lngCirculos.get(i));
+                    addCirculo(new LatLng(Double.parseDouble( latCirculos.get(i)),
+                            Double.parseDouble(latCirculos.get(i))));
+                    i++;
+                }
+        }
+        moveCamara();
     }
 
-    private void cambiarOpciones()
-    {
-        Mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        Mapa.getUiSettings().setZoomControlsEnabled(true);
+    private void addCirculo(LatLng point){
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(point);
+        latCirculos.add(point.latitude + "");
+        lngCirculos.add(point.longitude + "");
+
+        if (color.equals("Negro")) {
+            circleOptions.fillColor(Color.HSVToColor(75, new float[]{Color.BLACK, 1, 1}));
+            Log.d("COLOR",color);
+        } else if (color.equals("Azul")) {
+            circleOptions.fillColor(Color.HSVToColor(75, new float[]{Color.BLUE, 1, 1}));
+            Log.d("COLOR",color);
+        } else if (color.equals("Verde")) {
+            circleOptions.fillColor(Color.HSVToColor(75, new float[]{Color.GREEN, 1, 1}));
+            Log.d("COLOR",color);
+        } else if (color.equals("Rojo")) {
+            circleOptions.fillColor(Color.HSVToColor(75, new float[]{Color.RED, 1, 1}));
+            Log.d("COLOR",color);
+        }
+        circleOptions.radius(Integer.parseInt(tamanio));
+        circleOptions.strokeWidth(1);
+        Circle circulo = Mapa.addCircle(circleOptions);
     }
 
-    private void irUES()
-    {
-        CameraUpdate camUpd1 =
-                CameraUpdateFactory
-                        .newLatLngZoom(new LatLng(13.970263, -89.574808), 16);
+    private void mapType(){
+        if (mapa.equals("Normal")) {
+            Mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            Mapa.getUiSettings().setZoomControlsEnabled(true);
+        } else if (mapa.equals("Satelite")) {
+            Mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            Mapa.getUiSettings().setZoomControlsEnabled(true);
+        } else if (mapa.equals("Terreno")) {
+            Mapa.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            Mapa.getUiSettings().setZoomControlsEnabled(true);
+        } else if (mapa.equals("Hibrido")) {
+            Mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            Mapa.getUiSettings().setZoomControlsEnabled(true);
+        }
+    }
 
+    private void irPosicion() {
+        CameraUpdate camUpd1 = CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 16);
         Mapa.moveCamera(camUpd1);
     }
 
-
-    private void obtenerPosicion()
-    {
-        CameraPosition camPosicion = Mapa.getCameraPosition();
-
-        LatLng coordenadas = camPosicion.target;
-        double latitud = coordenadas.latitude;
-        double longitud = coordenadas.longitude;
-
-        Toast.makeText(this, "Lat: " + latitud + " | Long: " + longitud, Toast.LENGTH_SHORT).show();
+    public void moveCamara(){
+        Mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat,lng),16));
     }
 
+    public void guardar(){
+        if (latCirculos.size() > 0){
+            latCirculo = new HashSet<String>(latCirculos);
+            lngCirculo = new HashSet<String>(lngCirculos);
+        }
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putStringSet("latCirculo", latCirculo);
+        editor.putStringSet("lngCirculo", lngCirculo);
+        editor.putString("latitud", lat+"");
+        editor.putString("longitud", lng+"");
+        editor.commit();
+    }
 
+    private void irUES() {
+        CameraUpdate camUpd1 =
+                CameraUpdateFactory
+                        .newLatLngZoom(new LatLng(13.970263, -89.574808), 16);
+        Mapa.moveCamera(camUpd1);
+    }
 
+    private void obtenerPosicion() {
+        CameraPosition camPosicion = Mapa.getCameraPosition();
+        LatLng coordenadas = camPosicion.target;
+        lat = coordenadas.latitude;
+        lng = coordenadas.longitude;
+        Toast.makeText(this, "Lat: " + lat + " | Long: " + lng, Toast.LENGTH_SHORT).show();
+    }
 
     private void agregarPunto() {
-        Mapa.addMarker(new MarkerOptions()
-                .position(new LatLng(13.970263, -89.574808))
+        Mapa.addMarker(new MarkerOptions().position(new LatLng(13.970263, -89.574808))
                 .title("Santa Ana: UES"));
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MI_PERMISO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    Mapa.setMyLocationEnabled(true);
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                mapa = data.getStringExtra("tipo");
+                tamanio = data.getStringExtra("tamanio");
+                color = data.getStringExtra("color");
+                mapType();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        obtenerPosicion();
+        guardar();
+        super.onDestroy();
+    }
 }
--- app/src/main/java/sv/edu/uesocc/ipam2018/mapasandroid/OpcionesMapa.java
+++ app/src/main/java/sv/edu/uesocc/ipam2018/mapasandroid/OpcionesMapa.java
@@ -1,14 +1,81 @@
 package sv.edu.uesocc.ipam2018.mapasandroid;
 
-import android.preference.PreferenceActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class OpcionesMapa extends PreferenceActivity {
+public class OpcionesMapa extends AppCompatActivity {
+
+    private Spinner spMapa;
+    private Spinner spColor;
+    private EditText txtTamanio;
+    private Button btnGuardar;
+    private SharedPreferences preferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferencias);
+        setContentView(R.layout.activity_opciones_mapa);
+        preferences = getSharedPreferences("map_preferences",MODE_PRIVATE);
+
+        spMapa = (Spinner)findViewById(R.id.spTipoMapa);
+        spColor = (Spinner)findViewById(R.id.spColorForma);
+        txtTamanio = (EditText) findViewById(R.id.etTamaño);
+        btnGuardar = (Button) findViewById(R.id.btnGuardar);
+
+        if (preferences.getString("tipo","").equals("Normal")){
+            spMapa.setSelection(0);
+        } else if (preferences.getString("tipo","").equals("Satelital")){
+            spMapa.setSelection(1);
+        } else if (preferences.getString("tipo","").equals("Terreno")){
+            spMapa.setSelection(2);
+        } else if (preferences.getString("tipo","").equals("Hibrido")){
+            spMapa.setSelection(3);
+        }
+
+        if (preferences.getString("color","").equals("Negro")){
+            spColor.setSelection(0);
+        } else if (preferences.getString("color","").equals("Azul")){
+            spColor.setSelection(1);
+        } else if (preferences.getString("color","").equals("Verde")){
+            spColor.setSelection(2);
+        } else if (preferences.getString("color","").equals("Rojo")){
+            spColor.setSelection(3);
+        }
+
+        txtTamanio.setText(preferences.getString("tamanio","3000"));
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mapa = spMapa.getSelectedItem().toString();
+                String color = spColor.getSelectedItem().toString();
+                String tamanio = txtTamanio.getText().toString();
+
+                if ((tamanio != "") && (mapa != "") && (color != "") && (tamanio != null) && (mapa != null) && (color != null)){
+                    Intent intent = new Intent();
+                    intent.putExtra("tipo", mapa);
+                    intent.putExtra("color",color);
+                    intent.putExtra("tamanio",tamanio);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }else{
+                    Toast.makeText(OpcionesMapa.this,"Debe de llenar los campos requeridos", Toast.LENGTH_SHORT).show();
+                }
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("tipo", mapa);
+                editor.putString("color", color);
+                editor.putString("tamanio", tamanio);
+                editor.commit();
+                Intent intent = new Intent(OpcionesMapa.this, MainActivity.class);
+            }
+        });
     }
 }
